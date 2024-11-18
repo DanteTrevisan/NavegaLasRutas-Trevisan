e441f03692cc835331623244ff95a1425b8b025a
Dante Trevisan <dante.trevisan248@gmail.com>
2024-11-18 02:24:21 -0300

    terminados componentes y styles

---
+++ src/components/ItemListContainer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface ItemListContainerProps {
+    greeting: string
+}
+
+const ItemListContainer: React.FC<ItemListContainerProps> = ({ greeting }) => {
+    return(
+        <p>{greeting}</p>
+    )
+};
+
+export default ItemListContainer
