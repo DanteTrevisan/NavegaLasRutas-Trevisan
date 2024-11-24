818b882c8fb61f5509b5a9cbbf7f1f0f56352c83
Dante Trevisan <dante.trevisan248@gmail.com>
2024-11-24 10:52:22 -0300

    arreglado enlaces y comence con el carrito

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -3,10 +3,29 @@ import { urls } from "../utils/routes"
 
 const Cart = () => {
     return(
-        <section>
-            <p>Este es el carrito</p>
-        
+        <section className="flex flex-col justify-center items-center m-5">
+            <h2 className="font-semibold">COntenido del Carrito</h2>
+            <ul>
+                <p>ACA DEBERIA IR LO QUE TIENE EL CARRITO PERO TENGO QUE VER BIEN COMO SE HACE</p>
+                <p>ACA DEBERIA IR LO QUE TIENE EL CARRITO PERO TENGO QUE VER BIEN COMO SE HACE</p>
+                <p>ACA DEBERIA IR LO QUE TIENE EL CARRITO PERO TENGO QUE VER BIEN COMO SE HACE</p>
+                <p>ACA DEBERIA IR LO QUE TIENE EL CARRITO PERO TENGO QUE VER BIEN COMO SE HACE</p>
+                <p>ACA DEBERIA IR LO QUE TIENE EL CARRITO PERO TENGO QUE VER BIEN COMO SE HACE</p>
+                <p>ACA DEBERIA IR LO QUE TIENE EL CARRITO PERO TENGO QUE VER BIEN COMO SE HACE</p>
+                <p>ACA DEBERIA IR LO QUE TIENE EL CARRITO PERO TENGO QUE VER BIEN COMO SE HACE</p>
+                <p>ACA DEBERIA IR LO QUE TIENE EL CARRITO PERO TENGO QUE VER BIEN COMO SE HACE</p>
+                <p>ACA DEBERIA IR LO QUE TIENE EL CARRITO PERO TENGO QUE VER BIEN COMO SE HACE</p>
+                <p>ACA DEBERIA IR LO QUE TIENE EL CARRITO PERO TENGO QUE VER BIEN COMO SE HACE</p>
+            </ul>
+
+            <div className="flex gap-2">
+                <h4 className="font-sans">Precio Total $</h4>
+                <h4 className="font-semibold">ACA LLAMO LA FUNCION PARA CALCULAR EL PRECIO TOTAL PERO NO SE COMO SE HACE</h4>
+            </div>
+
+            <p>ALGUN BOTON PARA VARCIAR EL CARRITO/TERMINAR DE COMPRAR</p>
             <Link
+                className="bg-amber-900 text-amber-50 font-semibold px-2 py-1 rounded-md mt-4"
                 to={urls.checkout}
             >
             Finalizar Compra
