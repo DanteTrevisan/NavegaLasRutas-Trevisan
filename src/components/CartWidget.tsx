6ee7de32f022f40ad1b04ef77fd7842b092a1a08
Dante Trevisan <eldanteh@live.com>
2024-11-27 20:14:54 -0300

    terminado context.tsx

--- src/components/CartWidget.tsx
+++ src/components/CartWidget.tsx
@@ -1,10 +1,10 @@
 import cartIcon from "../assets/icons/cart.svg";
 import { Link } from "react-router-dom";
 import { urls } from "../utils/routes";
+import { CartContext } from "../context/CartContext";
+import { useContext } from "react";
 const CartWidget =  () => {
-    
-        
-
+        const { itemsInCart} = useContext(CartContext)
         return(
             <div className="flex justify-center items-center">
                 <Link
@@ -12,6 +12,7 @@ const CartWidget =  () => {
                     to={urls.cart}
                 >
                     <img src={cartIcon} alt="Cart Icon" className="w-6 m-1 text-amber-100" />
+                    {itemsInCart()}
                 </Link>
             </div>
     )
--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -33,3 +33,61 @@ interface CartProviderProps{
     children: ReactNode;
 }
 
+//Cart Context Provider
+export const CartProvider: React.FC<CartProviderProps> = ({ children }) => {
+    const [cart, setCart] = useState<ItemData[]>([])
+
+    /**
+     * @param {ItemData} item - Item a añadir al carro
+     */
+    const addItemToCart = (item:ItemData) =>{
+        setCart([...cart, item])
+    }
+
+    /**
+     * Carro vacio
+     */
+    const clearCart = () =>{
+        setCart([])
+    }
+
+    /**
+     * @returns {number} - Cantidad de items en el carrito
+     */
+    const itemsInCart = () => {
+        return cart.reduce((acc,item) => acc + item.quantity,0)
+    }
+
+    /**
+     * @param {number} id - item id
+     * @returns {boolean}
+     */
+    const isInCart = (id:number): boolean =>{
+        return cart.some((item) => item.id === id)
+    }
+
+    /**
+     * @returns {number} - Precio total acumulado
+     */
+    const totalPrice = () => {
+        return cart.reduce((acc,item) => acc + item.quantity * item.price,0)
+    }
+
+    const CartContextValue: CartContextType = {
+        cart,
+        setCart,
+        addItemToCart,
+        clearCart,
+        itemsInCart,
+        isInCart,
+        totalPrice,
+    };
+
+    return (
+        <CartContext.Provider value = {CartContextValue}>
+            {children}
+        </CartContext.Provider>
+    )
+    
+}
+
