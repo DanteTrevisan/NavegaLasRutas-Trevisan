537895aff27fdc2829cbe3e5f46017b9e04892a3
Dante Trevisan <dante.trevisan248@gmail.com>
2024-12-03 11:00:49 -0300

    finalizados detalles esteticos

--- src/components/ItemDetail.tsx
+++ src/components/ItemDetail.tsx
@@ -51,7 +51,9 @@ const ItemDetail : React.FC<ItemDetailProps> = ({ item }) => {
             </div>
 
             {!isInCart(item.id) ? (
-                <div>
+                <div className="flex gap-4 mb-4">
+                    <p>Selecciones la cantidad de copias que desea llevar:</p>
+
                     <ItemQuantitySelector
                         cantidad={cantidad}
                         setCantidad={setCantidad}
